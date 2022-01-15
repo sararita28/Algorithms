e937ac92b4c325cb89b54ca172b7735ea9d88676
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-01-15 06:06:37 -0500

    Update and rename I. Arrays.md to I. Arrays.js

--- I. Arrays.js
+++ I. Arrays.js
@@ -1,9 +1,5 @@
-### Implementation of arrays
-
 class Array { 
-
     constructor() {
-    
         this.length = 0;
         this.data = {}
     }
@@ -49,43 +45,44 @@ class Array {
 }
 
 
-## Time Complexity:
+/* Time Complexity:
 
-# Accessing an Element
+ Accessing an Element
   If we know the index: O(1)
   
-# Searching for an element Linearly
-arr.indexOf('X') </br>
-Time Complexity: O(n)  </br>
+ Searching for an element Linearly
+arr.indexOf('X') 
+Time Complexity: O(n)  
 why: the only way to find the index is by going through the array starting from the first element until it finds an element that has the value ‘X’
 
-# Adding an element
-  //To the end of the array </br>
-  arr.push(x) </br>
-  Time Complexity: O(1) </br>
-  why: All it does is add an element and give it an index that’s 1 greater than the index of the last element in the array. </br>
-  //To the beginning of the array </br>
-  arr.unshift(x) </br>
-  Time Complexity: O(n) </br>
-  why: Adding an element at the beginning of an array means the new element will have an index of 0. Which means that the index of every other element must be incremented by 1. </br>
-  //to any position of the array </br>
-  arr.splice() </br>
+ Adding an element
+  //To the end of the array 
+  arr.push(x) 
+  Time Complexity: O(1) 
+  why: All it does is add an element and give it an index that’s 1 greater than the index of the last element in the array. 
+  //To the beginning of the array 
+  arr.unshift(x) 
+  Time Complexity: O(n) 
+  why: Adding an element at the beginning of an array means the new element will have an index of 0. Which means that the index of every other element must be incremented by 1. 
+  //to any position of the array 
+  arr.splice() 
   Time Complexity: O(n)
   
   
-# Removing an element
-  //at the end of the array </br>
-  arr.pop() </br>
-  Time Complexity: O(1) </br>
-  //at the start of the array </br>
-  arr.shift() </br>
-  Time Complexity: O(n) </br>
-  //at any position of the array </br>
-  arr.splice() </br>
+ Removing an element
+  //at the end of the array 
+  arr.pop() 
+  Time Complexity: O(1) 
+  //at the start of the array 
+  arr.shift() 
+  Time Complexity: O(n) 
+  //at any position of the array 
+  arr.splice() 
   Time Complexity: O(n)
   
-# Concatenating two arrays
-  arr.concat(arr2) </br>
-  Time Complexity: O(n+m) (n depends on arr' length while m on arr2' length) </br>
+ Concatenating two arrays
+  arr.concat(arr2) 
+  Time Complexity: O(n+m) (n depends on arr' length while m on arr2' length) 
   
 Other methods like Array.filter(), Array.map(), Array.find(), Array.findIndex(), Array.reduce(), Array.forEach() have a time complexity of O(n)
+*/
