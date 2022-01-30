83f3b45dd522039781a8a56d22d8823c5fc96c17
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-01-30 15:09:10 -0500

    Update DoublyLinked-Lists.js

--- Data Structures/Linked Lists/DoublyLinked-Lists.js
+++ Data Structures/Linked Lists/DoublyLinked-Lists.js
@@ -1,86 +1,123 @@
-//Heavily flawed code. Fix!!!!
-
 class Node {
-    constructor(data, next, prev) {
-        this.data = data;
-        this.next = next;
-        this.prev = prev;
-    }
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
 }
 
 class LinkedList {
   constructor() {
     this.head = null;
-    this.length = 0;
     this.tail = null;
+    this.length = 0;
   }
 
-  //Inserting at head
-  insertAtHead(data) {
-    const newNode = new Node(data, this.head, null);
-    //this.head.prev = newNode;
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
-  }
-
-  //Inserting at tail
   insertAtTail(data) {
-    if (!this.head) return this.insertAtHead(data);
+    let newNode = new Node(data);
+    //if list is not empty
     if (this.tail) {
-        this.tail.next = new Node(data, null, this.tail)
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+      this.length++
+      return newNode;
     }
+
+    this.head = this.tail = newNode;
     this.length++;
+    return newNode;
   }
 
-  //Inserting anywhere in the list (at index)
-  insertAtIndex(index, data) {
-    if (index === 0) return this.insertAtHead(data);
-    
-    let prev = this.getByIndex(index - 1);
-    if (prev == null) return null;
-    const newNode = new Node(data, prev.next, prev);
-    prev.next = newNode
-    prev.next.prev = newNode;
-    this.length++;
-  }
-
-  //Searching for a node by index
-  getByIndex(index) {
-    if (index < 0 || index >= this.length) return null; //if index is not valid immediately return null
-
-    let current = this.head; //set current index to head
-    for (let i = 0; i < index; i++) {
-      //loop through till the index
-      current = current.next;
-    }
-    return current.data; //since the last element in the loop is pointing at the index (you can also return current if you need all the information)
-  }
-
-  //Removing at head
-  removeAtHead() {
-    this.head = this.head.next;
-    this.head.prev = null
-    this.length--;
-  }
-
-  //Removing at tail
   removeAtTail() {
-    if (this.head == null || this.head.next == null) return null;
-    this.tail = this.tail.prev
-    this.tail.next = null
-    this.length--;
+    if (this.tail) {
+      this.length--;
+
+      const removedTail = this.tail;
+
+      this.tail = this.tail.prev;
+      if (this.tail) {
+        this.tail.next = null;
+      } else {
+        this.head = null;
+      }
+
+      return removedTail;
+    }
+    return undefined;
   }
 
-  //Removing anywhere in the list (at index)
-  removeAtIndex(index) {
+  insertAtHead(data) {
+    this.length++;
+    let newNode = new Node(data);
+
+    if (this.head) {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+      return newNode;
+    }
+
+    this.head = this.tail = newNode;
+    return newNode;
+  }
+
+  removeAtHead() {
+    if (this.head) {
+      this.length--;
+      const removedHead = this.head;
+      this.head = this.head.next;
+
+      if (this.head) {
+        this.head.prev = null;
+      } else {
+        this.tail = null;
+      }
+
+      return removedHead;
+    }
+    return undefined;
+  }
+
+  insertIndex(data, index) {
+    if (index >= this.length) {
+      throw new Error("Insert index out of bounds");
+    }
+
+    if (index === 0) {
+      return this.insertAtHead(data);
+    }
+
+    this.length++;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    const prevNode = currentNode.prev;
+    const newNode = createNode(data);
+    newNode.next = currentNode;
+    newNode.prev = prevNode;
+    prevNode.next = newNode;
+    currentNode.prev = newNode;
+    return newNode;
+  }
+
+  // remove at specific index
+
+  removeIndex(index) {
+    if (index >= this.length) return null
+
     if (index === 0) return this.removeAtHead();
 
-    const prev = this.getByIndex(index - 1);
-    if (prev == null) return null;
-
-    prev.next = prev.next.next;
-    prev.next.prev = prev
     this.length--;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    const prevNode = currentNode.prev;
+    const nextNode = currentNode.next;
+    prevNode.next = nextNode;
+    nextNode.prev = prevNode;
+    return currentNode;
   }
 }
