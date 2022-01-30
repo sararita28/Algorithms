9dc35a546d13065b51935f9fb8143fb57890fc24
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-01-30 04:49:46 -0500

    Update Hash Tables.js

--- Data Structures/Hash Tables/Hash Tables.js
+++ Data Structures/Hash Tables/Hash Tables.js
@@ -1,76 +1,62 @@
-// create a hash table class (this example will use a table with the size of 127)
 class HashTable {
   constructor() {
-    this.table = new Array(127); //this is where all key/value pairs will be stored
+    this.table = [];
     this.size = 0;
   }
-  
-// create a hash() method to accept key value and transform it to an index
-  _hash(key) { // '_' indicates it's a private class
+
+  //Hash function
+  hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i); // this hash will sum the ASCII code of the chars in the key
+      hash += key.charCodeAt(i);
     }
-    return hash % this.table.length; //we use the modulo operator to ensure the value doesn't exceed the bucket size (of 127 in this case)
+    return hash % this.table.length;
   }
 
-  //write a set() method to get the index value
-  set(key, value) {
-    const index = this._hash(key); // call the _hash() method to get the index value
-    if (this.table[index]) {
+  //Inserting a key/value pair
+  setItem(key, value) {
+    let index = this.hash(key);
+    if (this.table[index]) { //if the position index is already filled
       for (let i = 0; i < this.table[index].length; i++) {
-        if (this.table[index][i][0] === key) {
-          this.table[index][i][1] = value;
+        if (this.table[index][i][0] === key) { //and the key is already there 
+          this.table[index][i][1] = value; //update its value
           return;
         }
       }
-      this.table[index].push([key, value]);
-    } else {
-      this.table[index] = [];
+      this.table[index].push([key, value]); // if the key is not there, you push it
+    } else { //if the position is not filled 
+      this.table[index] = []; 
       this.table[index].push([key, value]);
     }
     this.size++;
   }
 
-  //write a get() method to retrieve a value by its key
-  get(key) {
-    const index = this._hash(key);
+  //Searching for a value based on its key
+  getItem(key) {
+    let index = this.hash(key)
     if (this.table[index]) {
-      for (let i = 0; i < this.table.length; i++) {
-        if (this.table[index][i][0] === key) {
-          return this.table[index][i][1];
+        for (let i=0; i<this.table.length; i++) {
+            if (this.table[index][i][0] === key) { // if you've found the key
+                return this.table[index][i][1] //return its value
+            }
         }
-      }
     }
-    return undefined; // if no key/value pair is stored in the specific index return indefined
+    return undefined
   }
 
-  //write a remove() method to delete a key/value pair
-  remove(key) {
-    //retrieve the index using the _hash() method
-    const index = this._hash(key);
-
-    if (this.table[index] && this.table[index].length) {
-      for (let i = 0; i < this.table.length; i++) {
-        if (this.table[index][i][0] === key) {
-          this.table[index].splice(i, 1);
-          this.size--;
-          return true;
+  //Removing/Deleting a key/value pair
+  removeItem(key) {
+    let index = this.hash(key);
+    if (this.table[index] && this.table[index].length) { //if the index isn't empty
+        for (let i=0; i<this.table.length; i++) {
+            if (this.table[index][i][0] === key) {
+                this.table[index].splice(i, 1); //delete the  key/value pair
+                this.size--;
+                return true
+            }
         }
-      }
     } else {
-      return false;
+        return false
     }
-  }
-
-  display() {
-    this.table.forEach((values, index) => {
-      const chainedValues = values.map(
-        ([key, value]) => `[ ${key}: ${value} ]`
-      );
-      console.log(`${index}: ${chainedValues}`);
-    });
   }
 }
-
-//source: FreeCodeCamp
