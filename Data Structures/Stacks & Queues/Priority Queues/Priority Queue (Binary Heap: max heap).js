fc8b06cd94ab847ffc93c09adbfa4e32eaae687b
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-01-31 07:16:54 -0500

    Update Priority Queue (Binary Heap: max heap).js

--- Data Structures/Stacks & Queues/Priority Queues/Priority Queue (Binary Heap: max heap).js
+++ Data Structures/Stacks & Queues/Priority Queues/Priority Queue (Binary Heap: max heap).js
@@ -1,3 +1,115 @@
+//Return to this once you've studied Heaps
+
+class QueueElement {
+  constructor(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  //helper method to swap 2 items
+  swap(index1, index2) {
+    let temp = this.items[index1];
+    this.items[index1] = this.items[index2];
+    this.items[index2] = temp;
+    return this.items;
+  }
+
+  //helper method to bubble up element
+  bubbleUp() {
+    //get index of (last) inserted element
+    let index = this.items.length - 1;
+    //loop while index is not 0 or element no loger needs to bubble up
+    while (index > 0) {
+      //get parent index
+      let parentIndex = Math.floor((index - 1) / 2);
+      //if items is greater than parent, swap the two
+      if (this.items[parentIndex].priority > this.items[index].priority) {
+        //swap with helper method
+        this.swap(index, parentIndex);
+        //update index to be parent index
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+    return 0;
+  }
+
+  //helper method to bubble down element
+  bubbleDown() {
+    let parentIndex = 0;
+    const length = this.items.length;
+    const elementPriority = this.items[0].priority;
+    //loop breaks if no swaps are needed
+    while (true) {
+      //get indexes of child elements
+      let leftChildIndex = 2 * parentIndex + 1;
+      let rightChildIndex = 2 * parentIndex + 2;
+      let leftChildPriority, rightChildPriority;
+      let indexToSwap = null;
+      // if left child exists, and is greater than the element, plan to swap with the left child index
+      if (leftChildIndex < length) {
+        leftChildPriority = this.items[leftChildIndex].priority;
+        if (leftChildPriority < elementPriority) {
+          indexToSwap = leftChildIndex;
+        }
+      }
+      //if right child exists
+      if (rightChildIndex < length) {
+        rightChildPriority = this.items[rightChildIndex].priority;
+
+        if (
+          //if right child is greater than element and there are no plans to swap
+          (rightChildPriority < elementPriority && indexToSwap === null) ||
+          //OR if right child is greater than left child and there ARE plans to swap
+          (rightChildPriority < leftChildPriority && indexToSwap !== null)
+        ) {
+          //plan to swap with the right child
+          indexToSwap = rightChildIndex;
+        }
+      }
+      //if there are no plans to swap, break out of the loop
+      if (indexToSwap === null) {
+        break;
+      }
+      //swap with planned element
+      this.swap(parentIndex, indexToSwap);
+      //starting index is now index that we swapped with
+      parentIndex = indexToSwap;
+    }
+  }
+
+  // method that pushes new value onto the end and calls the bubble helper
+  enqueue(element) {
+    this.items.push(element);
+    //calculate parent, if parent is greater swap
+    //while loop or recurse
+    this.bubbleUp();
+    return this.items;
+  }
+
+  dequeue() {
+    //swap first and last element
+    this.swap(0, this.items.length - 1);
+    //pop max value off of items
+    let poppedNode = this.items.pop();
+    //re-adjust heap if length is greater than 1
+    if (this.items.length > 1) {
+      this.bubbleDown();
+    }
+
+    return poppedNode;
+  }
+}
+
+
+//-----------Alternative implementation-------------
 
 var H = Array(50).fill(0);  
 var size = -1;
