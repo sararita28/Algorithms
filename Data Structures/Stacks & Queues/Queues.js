25d112c2d7a45800af49bdb39acba98d4f7e324b
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-01-31 05:19:56 -0500

    Update Queues.js

--- Data Structures/Stacks & Queues/Queues.js
+++ Data Structures/Stacks & Queues/Queues.js
@@ -1,44 +1,75 @@
+class Queue {
+  constructor() {
+    this.items = [];
+    this.count = 0;
+  }
+
+  //Aadding to last
+  enqueue(element) {
+    this.count++;
+    this.items[this.count -1] = element;
+  }
+
+  //Removing first element and returning it
+  dequeue() {
+    if (this.count == 0) return `Queue is empty`;
+    this.items.shift();
+    this.count--;
+  }
+
+  //Peeking (returning top/head of queue)
+  peek() {
+    if (this.count == 0) return `Queue is empty`;
+    return this.items[0]
+  }
+
+  //Checking if queue is empty
+  isEmpty() {
+    return this.count == 0;
+  }
+
+  //getting length/size of queue
+  size() {
+    return this.count;
+  }
+}
+
+//------------------- Alternative Implementation -----------------------
 
 class Queue {
-    constructor() {
-        this.items = [];
-    }
+  constructor() {
+    this.items = [];
+  }
 
-    //adds an element to the queue
-    enqueue(element) {
-        this.items.push(element);
-    }
+  // adding element to the queue
+  enqueue(element) {
+    return this.items.push(element);
+  }
 
-    //removes an element from the queue
-    dequeue() {
-        if (this.isEmpty()) {
-            return "Underflow";
-        } else {
-            return this.items.shift();
-        }
+  // removing element from the queue
+  dequeue() {
+    if (this.items.length > 0) {
+      return this.items.shift();
     }
+  }
 
-    //returns the front element of the queue
-    front() {
-        if (this.isEmpty()) {
-            return "No elements in queue"
-        } else {
-        return this.items[0]
-        }
-    }
+  // viewing the last element
+  peek() {
+    return this.items[this.items.length - 1];
+  }
 
-    //checks if the queue is empty
-    isEmpty() {
-        return this.items.length === 0;
-    }
+  // check if the queue is empty
+  isEmpty() {
+    return this.items.length == 0;
+  }
 
-    //prints all elements of the queue (in a string)
-    printQueue() {
-        var str = ""
-        for (var i = 0; i < this.items.length; i++) {
-            str += this.items[i] + ' ';
-        }
-        return str;
-    }
+  // returning the size of the queue
+  size() {
+    return this.items.length;
+  }
 
+  // emptying the queue
+  clear() {
+    this.items = [];
+  }
 }
