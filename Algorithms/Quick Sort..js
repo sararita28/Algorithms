97f81987721e47cadf0554cd5814dffbed12596a
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-02-07 08:04:25 -0500

    Update Quick Sort..js

--- Algorithms/Quick Sort..js
+++ Algorithms/Quick Sort..js
@@ -4,14 +4,17 @@ Pick a random element and partition the array such that all numbers that are les
   come before all numbers that are greater than it. If we repeatedly partition the array (and its sub-arrays) around an element, the array will eventually become 
     sorted.
 
-Quick sort is a divide and conquer algorithms(i.e. recursive algorithms). In quicksort, the combine step doesn't do anything; the real work of quicksort happens during the divide step(in pivot selection to be precise), which partitions subarray array[p..r] around a pivot drawn from the subarray. Although we can choose any element in the subarray as the pivot, it's easy to implement partitioning if we choose the rightmost element of the subarray, A[r], as the pivot.
+Quick sort is a divide and conquer algorithms(i.e. recursive algorithms). In quicksort, the combine step doesn't do anything; the real work of quicksort 
+happens during the divide step(in pivot selection to be precise), which partitions subarray array[p..r] around a pivot drawn from the subarray. Although we 
+can choose any element in the subarray as the pivot, it's easy to implement partitioning if we choose the rightmost element of the subarray, A[r], as the pivot.
 
 In practice, quicksort outperforms merge sort, and it significantly outperforms selection sort and insertion sort.
 
 ---
 
 Keep in mind:
-In merge sort, you never see a subarray with no elements, but you can in quicksort, if the other elements in the subarray are all less than the pivot or all greater than the pivot.
+In merge sort, you never see a subarray with no elements, but you can in quicksort, if the other elements in the subarray are all less than the pivot or all 
+greater than the pivot.
 */
 
 
