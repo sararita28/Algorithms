52ed4c145dd8d80d9ccc1f8d46a20fc2d4a59ce5
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-02-07 07:40:33 -0500

    Update Merge Sort.js

--- Algorithms/Merge Sort.js
+++ Algorithms/Merge Sort.js
@@ -1,6 +1,10 @@
 /* Merge Sort:
 Runtime: O(nlogn) | Memory: Depends
-Merge sort is a divide and conquer algorithms(i.e. recursive algorithms). Divides the array in half, sorts each then merges them together. In merge sort, the divide step doesn't do anything important; it's the "merge" part that does all the heavy lifting. The merge method operates by copying all the elements from the target array segment into a helper array, keeping track of where the start of the left and right halves should be (helperleft and helperRight). We then iterate through helper, copying the smaller element from each half into the array. At the end, we copy any remaining elements into the target array. In merge sort, you never see a subarray with no elements.
+Merge sort is a divide and conquer algorithms(i.e. recursive algorithms). Divides the array in half, sorts each then merges them together. In merge sort, 
+the divide step doesn't do anything important; it's the "merge" part that does all the heavy lifting. The merge method operates by copying all the elements 
+from the target array segment into a helper array, keeping track of where the start of the left and right halves should be (helperleft and helperRight). 
+We then iterate through helper, copying the smaller element from each half into the array. At the end, we copy any remaining elements into the target array. 
+In merge sort, you never see a subarray with no elements.
 The steps of mergesort are : 
 1. Recursively split the input array in half until a subarray with only 1 element is produced.
 2. Merge each sorted subarray together to produce the final sorted array.
