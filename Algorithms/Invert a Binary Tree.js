7e39ff0af55771f25382b4b244f8c3d530cd649b
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-02-10 06:03:39 -0500

    Create Invert a Binary Tree.js

---
+++ Algorithms/Invert a Binary Tree.js
@@ -0,0 +1,14 @@
+//Recursively
+
+function invertTree(head) {
+  if (head) {
+    var temp = head.left;
+    head.left = head.right;
+    head.right = temp;
+
+    invertTree(head.left);
+    invertTree(head.right);
+  }
+
+  return head;
+}
