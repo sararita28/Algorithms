e160566f9ccff3db58a875c20fcac10208a70c77
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-02-10 06:14:23 -0500

    Create Reverse a Linked List(Singly) .js

---
+++ Algorithms/Reverse a Linked List(Singly) .js
@@ -0,0 +1,34 @@
+//Recursively  O(n) time & O(n) space
+
+function reverse(head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  let tmp = reverse(head.next);
+  head.next.next = head;
+  head.next = undefined;
+  return tmp;
+}
+
+//Iteratively  O(n) time & O(1) space
+
+
+function reverse(head) {
+  let node = head,
+      previous,
+      tmp;
+
+  while (node) {
+    // save next before we overwrite node.next!
+    tmp = node.next;
+
+    // reverse pointer
+    node.next = previous;
+
+    // step forward in the list
+    previous = node;
+    node = tmp;
+  }
+
+  return previous;
+}
