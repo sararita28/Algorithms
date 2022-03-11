944a0a159dfe04fcafcde216e1ca22b545eede5e
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-11 10:39:39 -0500

    Update Invert a Binary Tree.js

--- Must-Know/Invert a Binary Tree.js
+++ Must-Know/Invert a Binary Tree.js
@@ -1,6 +1,5 @@
 //Recursively
-
-function invertTree(head) {
+function invertBinaryTree(head) {
   if (head) {
     var temp = head.left;
     head.left = head.right;
@@ -9,6 +8,21 @@ function invertTree(head) {
     invertTree(head.left);
     invertTree(head.right);
   }
-
   return head;
 }
+
+
+//Iteratively (using pre-order traversal)
+function invertBinaryTree(root) {
+    if (root == null) return
+    let stack = [root]
+    while (stack.length > 0) {
+        // pop top node from stack
+        let current = stack.pop()
+
+        swap(current.left, current.right)
+        if (current.right) stack.push(current.right)
+        if (curr.left) stack.push(current.left)
+    }
+}
+
