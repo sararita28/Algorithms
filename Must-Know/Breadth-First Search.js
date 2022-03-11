6a4900a986ff80f19aecc989113e8d557f4125c0
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-11 08:30:57 -0500

    Update Breadth-First Search.js

--- Must-Know/Breadth-First Search.js
+++ Must-Know/Breadth-First Search.js
@@ -1,7 +1,9 @@
-function breadthFirstSearch(graph, startNode) {
+function bfs(graph, startNode) {
     const queue = [startNode]
     while (queue.length > 0) {
+        //remove the first element of the queue
         const current = queue.shift()
+        //visit the neighbors of that first element and add them to the queue
         for (let neighbor of graph[current]) {
             queue.push(neighbor)
         }
