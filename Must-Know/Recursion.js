43b250457c9044dbeba44a41b9d48b0095f194d7
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-11 08:13:28 -0500

    Update Recursion.js

--- Must-Know/Recursion.js
+++ Must-Know/Recursion.js
@@ -1,1 +1,10 @@
+//Example of a function that uses recursion. Base case is when n is 0. Function recursively calls itself till condition is met (i.e. n++ <10)
+let n = 0;
 
+function countUp() {
+  console.log(n);
+  if (n++ < 10) {
+    countUp(n);
+  }
+}
+countUp();
