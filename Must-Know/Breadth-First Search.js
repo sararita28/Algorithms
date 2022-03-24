7a094f15294659232b592f7e25fa5a84e4bcb78f
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-24 05:10:41 -0400

    Update Breadth-First Search.js

--- Must-Know/Breadth-First Search.js
+++ Must-Know/Breadth-First Search.js
@@ -1,3 +1,10 @@
+/*
+Uses a queue to keep track of all the nodes of a level before jumping onto the next level. Any problem involving the traversal of a tree in a 
+level-by-level order can be efficiently solved using this approach. The Tree BFS pattern works by pushing the root node to the queue and then 
+continually iterating until the queue is empty. For each iteration, we remove the node at the head of the queue and “visit” that node. 
+After removing each node from the queue, we also insert all of its children into the queue.
+*/
+
 function bfs(graph, startNode) {
     const queue = [startNode]
     while (queue.length > 0) {
