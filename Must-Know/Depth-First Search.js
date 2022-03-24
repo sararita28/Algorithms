37e54bf7804f76a2ca294c0f8f9046998131161d
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-24 04:45:34 -0400

    Update Depth-First Search.js

--- Must-Know/Depth-First Search.js
+++ Must-Know/Depth-First Search.js
@@ -1,3 +1,10 @@
+/*
+You can use recursion (or a stack for the iterative approach) to keep track of all the previous (parent) nodes while traversing.
+The Tree DFS pattern works by starting at the root of the tree, if the node is not a leaf you need to do three things:
+Decide whether to process the current node now (pre-order), or between processing two children (in-order) or after processing both children (post-order).
+Make two recursive calls for both the children of the current node to process them.
+
+*/
 
 //Iterative
 
