3f151a87659715e4cf2c9c4b1b8b89e346dc23b9
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-26 04:47:06 -0400

    Update and rename 2 heaps.js to Two heaps.js

---
+++ Must-Know/Two heaps.js
@@ -0,0 +1,5 @@
+/*
+To be able to solve these kinds of problems, we want to know the smallest element in one part and the biggest element in the other part. Two Heaps 
+pattern uses two Heap data structure to solve these problems; a Min Heap to find the smallest element and a Max Heap to find the biggest element.
+*/
+
