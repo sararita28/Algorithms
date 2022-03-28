3c5ce2ad3deaede1a723ebb084599fe1eeb1b9a9
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-03-28 05:39:35 -0400

    Update Breadth-First Search.js

--- Must-Know/Breadth-First Search.js
+++ Must-Know/Breadth-First Search.js
@@ -8,12 +8,10 @@ After removing each node from the queue, we also insert all of its children into
 function bfs(graph, startNode) {
     const queue = [startNode]
     while (queue.length > 0) {
-        //remove the first element of the queue
+        //store the first element of the queue in variable
         const current = queue.shift()
         //visit the neighbors of that first element and add them to the queue
-        for (let neighbor of graph[current]) {
-            queue.push(neighbor)
-        }
+        for (let neighbor of graph[current]) queue.push(neighbor)
         return current //returns nodes in order of viewing
     }
 }
