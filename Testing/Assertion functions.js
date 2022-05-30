da23b8d530e53b5b88c1f9bfde674d236d16b524
sararita28 <67070886+sararita28@users.noreply.github.com>
2022-05-30 14:01:17 +0100

    Update Assertion functions.js

--- Testing/Assertion functions.js
+++ Testing/Assertion functions.js
@@ -1,9 +1,29 @@
 // assertEqual
-// takes 3 parameters: actual (scalar; result of calling function), expected (scalar; theoretical result of calling function; i.e. what we expect it to return), testName (string; describes what a call to assertEqual is verifying about the function being tested.)
+// takes 3 parameters: actual (scalar; ideally result of calling function), expected (scalar; theoretical result of calling function; i.e. what we expect it to return), testName (string; describes what a call to assertEqual is verifying about the function being tested.)
 function assertEqual(actual, expected, testName) {
     if (actual === expected) {
      console.log('passed')   
     } else {
      console.log('FAILED ['+testName+'] Expected "'+expected+'", but got "'+actual+'"')
     }
+}
+
+// assertArraysEqual
+// takes 3 parameters: actual (array of scalars; ideally result of calling function), expected (array of scalars; theoretical result of calling function; i.e. what we expect it to return), testName (string; describes what a call to assertArraysEqual is verifying about the function being tested.)
+function assertArraysEqual(actual, expected, testName) {
+  let sameLength = actual.length === expected.length
+  let sameValue = true
+  for (let i in expected) {
+    if (expected[i] !== actual[i]) {
+      sameValue = false
+      break;
+    }
+  }
+
+  if (sameLength && sameValue) {
+    console.log('passed')
+  } else {
+    console.log('FAILED [' + testName + '] Expected "'+ expected + '", but got "' + actual + '"')
+
+  }
 }
